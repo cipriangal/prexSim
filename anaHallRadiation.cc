f9b920079361b2b6c6f877677bb6c801380ddec2
Ciprian Gal <ciprian@jlab.org>
2016-02-22 10:34:04 -0500

    update to analyzing multiple detectors at once

--- anaHallRadiation.cc
+++ anaHallRadiation.cc
@@ -13,6 +13,8 @@
 #include <TChain.h>
 #include <TDirectory.h>
 
+using namespace std;
+
 //Radiation All Vertices
 TH1F *Histo_vert_z_full[3];//particle type = photons, electrons, neutrons
 //2D histograms
@@ -30,69 +32,52 @@ Double_t flux[3][3],power[3][3];
 Double_t flux_range[3][3][3],power_range[3][3][3];
 float tot_events;
 TDirectory* gD;
-TString prename;
+string prename;
 
-float SensVolume_v;
+int SensVolume_v;
 //8002 cylindircal det at the radius close to electronic hut
 //8003 cyl det at the wall
 
 void bookHisto();
-void processTree(TString tname);
+void processTree(string tname);
 void Init();
-void WriteHisto(TString fname);
+void WriteHisto(string fname);
 
-
-using namespace std;
 
 int main(int argc,char** argv) {
 
   if(argc < 3){
-    cout<<"Usage: anaRad [sensative detector #] [output filename] [additional file names]"<<endl;
-    cout<<"   Output files should be of the type: path/Prename_*.root"<<endl;
+    cout<<"Usage: anaRad [input file name] [output file name] [list of sensative detectors #]"<<endl;
+    cout<<"   Output files will be of the type: output/anaRad_OutputFileName.root"<<endl;
     return 1;
   }
-  TString _sensVol(argv[1]);
-  SensVolume_v=_sensVol.Atof();
-  
-  if(!(SensVolume_v==2001 || SensVolume_v==2002 ||
-       SensVolume_v==10008 || SensVolume_v==10009 ||
-       SensVolume_v==8003 ||SensVolume_v==8004 ||
-       SensVolume_v==8005 || SensVolume_v==10001 ||
-       SensVolume_v==10002 || SensVolume_v==10003 ||
-       SensVolume_v==10004 )) {
-    cout<<" ~~~ You are asking for detector "<<SensVolume_v<<endl;
-    cout<<" This code does not work for that detector. Please modify."<<endl;
-    return 1;
-  }
-  cout<<endl;
-  cout<<"~~~~ Sensative volue set to :" <<SensVolume_v<<endl;
-  cout<<endl;
-  
-  TString rfile(Form("output/o_xx_%d_Radiation.root",(int)SensVolume_v));
-  TFile *rf=new TFile(rfile.Data(),"RECREATE");
+  string ifnm=argv[1]; 
+  string ofnm=argv[2];
+  TFile *rf=new TFile(Form("output/anaRad_%s.root",ofnm.c_str()),"RECREATE");
   rf->Close();
 
-  Init();
-  bookHisto();
-  
-  /* basic way to process one sim file
-  TString inFile="";
-  processTree(inFile);
-  prename="";
-  WriteHisto(rfile);
-  */
-  for(int i=2;i<argc;i++){
+  map <int,string> SensNames;
+  SensNames[10008]="HRShut";
+  SensNames[10009]="Septum";
+  SensNames[2001] ="Lpower";
+  SensNames[2002] ="Rpower";
+
+  for(int i=3;i<argc;i++){
+    TString _sensVol(argv[i]);
+    SensVolume_v=_sensVol.Atoi();
+    cout<<"~~~~ Sensative volue set to :" <<SensVolume_v<<endl;
+    cout<<endl;
+
+    prename=Form("SV%d",SensVolume_v);
+    if( SensNames.find(SensVolume_v)!=SensNames.end() )
+      prename=SensNames[SensVolume_v];
+
+    bookHisto();
     Init();
-    TString inFile(argv[i]);
-    prename=inFile;
-    prename.Remove(0,prename.First("/")+1);
-    prename.Remove(prename.First("_"),prename.Length());
-    prename+="_"+_sensVol;
-    cout<<"Processing "<<inFile.Data()<<" with prename "<<prename<<endl;
-    processTree(inFile);
-    WriteHisto(rfile);
+    processTree(ifnm);
+    WriteHisto(Form("output/anaRad_%s.root",ofnm.c_str()));
   }
-  
+    
   return 0;
 }
 
@@ -104,15 +89,14 @@ void Init(){
   up_ranges[1]= 135;
   up_ranges[2]= 3400;
   
-  
   for(int i=0;i<3;i++){
     if(Histo_vert_z_full[i]){
       Histo_vert_z_full[i]          ->Reset();
       Histo_vert_2D[i]              ->Reset();
       Histo_vert_full_2D[i]         ->Reset();
       Histo_vert_z_weighted_full[i] ->Reset();
-      Histo_Energy_lt_10[i]          ->Reset();
-      Histo_Energy_gt_10[i]          ->Reset();
+      Histo_Energy_lt_10[i]         ->Reset();
+      Histo_Energy_gt_10[i]         ->Reset();
     }
     
     for(int j=0;j<3;j++){
@@ -120,10 +104,11 @@ void Init(){
       power[i][j]=0.0;
       
       if(Histo_vert_z_full[i]){
-	Histo_vert_z[i][j]            ->Reset();
-	Histo_vert_z_weighted[i][j]   ->Reset();
+      	Histo_vert_z[i][j]             ->Reset();
+	Histo_vert_z_weighted[i][j]    ->Reset();
 	Histo_Energy_custom_lt_10[i][j]->Reset();
 	Histo_Energy_custom_gt_10[i][j]->Reset();
+
       }
       
       for(int k=0;k<3;k++){
@@ -133,8 +118,7 @@ void Init(){
     }
   }
 
-  if(Det_Face) Det_Face  ->Reset();
-
+  if(Det_Face)  Det_Face  ->Reset();
 }
  
 void bookHisto(){
@@ -183,7 +167,7 @@ void PrintInfo(){
   TString eName[3]={"  0<E<0.1","0.1<E<10"," 10<E<1000"};
   TString zName[3]={"-2600<z<-110"," -110<z<135","  135<z<3400"};
   cout<<" ~Printing:"<<endl;
-  ofstream fout(Form("output/o_%s_powerFlux.dat",prename.Data()),std::ofstream::out);
+  ofstream fout(Form("output/o_%s_powerFlux.dat",prename.c_str()),std::ofstream::out);
   
   for(int i=0;i<3;i++)
     for(int j=0;j<3;j++)
@@ -205,7 +189,7 @@ void PrintInfo(){
 
 }
 
-void processTree(TString tname){
+void processTree(string tname){
 
   Float_t kineE, type, volume, track, parent, PDGid, Edeposit, event, creator;
   //New Float for simulation of various detectors
@@ -213,8 +197,8 @@ void processTree(TString tname){
   Float_t px,py,pz;
   Float_t x_0,y_0,z_0,xd,yd,zd;
   
-  TChain * t = new TChain("geant");
-  t->Add(tname.Data());
+  TChain *t = new TChain("geant");
+  t->Add(tname.c_str());
   t->SetBranchAddress("type",&type);
   t->SetBranchAddress("volume",&volume);
   t->SetBranchAddress("px",&px);
@@ -253,6 +237,7 @@ void processTree(TString tname){
   Int_t nentries = (Int_t)t->GetEntries();
   for (int i=0; i<nentries ; i++) {
     t->GetEntry(i);
+
     hit_radius = sqrt(pow(xd/10,2)+pow(yd/10,2));
      
     if ( volume==SensVolume_v  && z_0 > -26000  && type<=5 && hit_radius > hit_radius_min ){
@@ -304,8 +289,9 @@ void processTree(TString tname){
     }
     
        
-    if(i % 1000000 == 1 ) cout<<" processed: "<<tname.Data()<<" "<<i<<endl;
+    if(i % 1000000 == 1 ) cout<<" processed: "<<tname.c_str()<<" "<<i<<endl;
   }
+
   for (int i=0; i<nentries; i++) {
     t->GetEntry(i);
     if  ( volume==SensVolume_v  && z_0 > -26000  && type<=5){
@@ -345,25 +331,26 @@ void processTree(TString tname){
     }
   
   PrintInfo();
+  delete t;
 }
 
 void writeEachHisto(TH1F* a){
-  a -> SetName(Form("%s_%s",prename.Data(),a->GetName()));
-  a -> SetTitle(Form("%s %s",prename.Data(),a->GetTitle()));
+  a -> SetName(Form("%s_%s",prename.c_str(),a->GetName()));
+  a -> SetTitle(Form("%s %s",prename.c_str(),a->GetTitle()));
   a -> SetDirectory(gD);
   a -> Write();
 }
 
 void writeEachHisto(TH2F* a){
-  a -> SetName(Form("%s_%s",prename.Data(),a->GetName()));
-  a -> SetTitle(Form("%s %s",prename.Data(),a->GetTitle()));
+  a -> SetName(Form("%s_%s",prename.c_str(),a->GetName()));
+  a -> SetTitle(Form("%s %s",prename.c_str(),a->GetTitle()));
   a -> SetDirectory(gD);
   a -> Write();
 }
 
-void WriteHisto(TString fname){
+void WriteHisto(string fname){
   
-  TFile *fout=TFile::Open(fname.Data(),"UPDATE");
+  TFile *fout=TFile::Open(fname.c_str(),"UPDATE");
   gD=fout->GetDirectory("");
   
   for(int i=0;i<3;i++){
@@ -384,5 +371,6 @@ void WriteHisto(TString fname){
   writeEachHisto(Det_Face);
   
   fout->Close();
+  delete fout;
 }
 
