ea653ae06024367970e21da258b70ec7663ecac1
Ciprian_Gal <Ciprian Gal>
2017-04-07 11:26:56 -0400

    update script: add schema folder, kripDet. this version produced statistically independent results

--- scripts/rivannaSubmit.py
+++ scripts/rivannaSubmit.py
@@ -5,14 +5,14 @@ import sys,os,time
 def main():
 
     ### always check that these are the options you want
-    sourceDir=""
-    outputDir=""
-    email=""
-    submit=0
+    sourceDir="/home/cg8fd/prex/prexSim"
+    outputDir="/home/cg8fd/prex/prexSim/output"
+    email="cg8fd@virginia.edu"
+    submit=1
 
     ##only crex5 defined for now
     configuration="crex5"
-    nrEv=10000
+    nrEv=100
     nrStart=0
     nrStop =5
 
@@ -21,8 +21,8 @@ def main():
     for nr in range(nrStart,nrStop): # repeat for nr jobs
         print "Starting job setup for jobID:",nr
         
-        jobFullName=jobName+"_"+'_%04d'%nr
-        createMacFiles(configuration,outputDir+"/"+jobName,nrEv,nr)
+        jobFullName=jobName+'_%04d'%nr
+        createMacFiles(configuration,outputDir+"/"+jobFullName,sourceDir,nrEv,nr)
 
         ###copy executable
         call(["cp",sourceDir+"/build/prexsim",
@@ -42,9 +42,14 @@ def main():
     
 def createMacFiles(config,outDir,sourceDir,nrEv,jobNr):
 
-    call(["cp",sourceDir+"/geometry/materials.xml",outDir+"/"])
+    if not os.path.exists(outDir+"/geometry"):
+        os.makedirs(outDir+"/geometry")
+
+    call(["cp",sourceDir+"/geometry/materials.xml",outDir+"/geometry/"])
+    call(["cp",sourceDir+"/geometry/kriptoniteDetectors.gdml",outDir+"/geometry/"])
+    call(["cp","-r",sourceDir+"/geometry/schema",outDir+"/geometry"])    
     if config=="crex5":
-        call(["cp",sourceDir+"/geometry/crex5deg.xml",outDir+"/"])
+        call(["cp",sourceDir+"/geometry/crex5deg.gdml",outDir+"/geometry"])
     
     f=open(outDir+"/"+"/myRun.mac",'w')
     f.write("/moller/ana/rootfilename ./o_prexSim.root\n")
@@ -61,10 +66,10 @@ def createMacFiles(config,outDir,sourceDir,nrEv,jobNr):
 
     if config=="crex5":
         f.write("/gun/energy 2.2 GeV\n")
-        f.write(" /moller/field/setFieldScaleFactor 2.35\n")
+        f.write("/moller/field/setFieldScaleFactor 2.35\n")
         f.write("/moller/field/setLowLim -74 cm\n")
         f.write("/moller/field/setHighLim 74 cm\n")
-        f.write("/moller/det/setDetectorFileName crex5deg.gdml\n")
+        f.write("/moller/det/setDetectorFileName geometry/crex5deg.gdml\n")
     
     f.write("/moller/det/setShieldMaterial polyethylene\n")
     f.write("/testhadr/CutsAll 0.7 mm\n")
@@ -84,7 +89,8 @@ def createBashScript(outDir):
     f.write("#SBATCH --mail-type=ALL\n")
     f.write("#SBATCH --mail-user=cg8fd@virginia.edu\n")
     f.write("#SBATCH --partition=serial\n")
-    f.write(outDir+"/prexsim "+outDir+"/preRun.mac"+outDir+"/myRun.mac"+"\n")
+    f.write("cd "+outDir+"\n")
+    f.write("./prexsim preRun.mac myRun.mac\n")
     f.close()
     return 0  
 
