3d4e6cc6ff9d01ab4f612f1cf8d630e94c464c95
Ciprian Gal <ciprian@jlab.org>
2017-05-03 10:42:49 -0400

    add b field configuration. use two different GetFieldValue methods and select which one gets used in a wrapper

--- include/MollerGlobalMagnetField.hh
+++ include/MollerGlobalMagnetField.hh
@@ -1,53 +1,3 @@
-//=============================================================================
-// 
-//   ---------------------------
-//  | Doxygen File Information |
-//  ---------------------------
-/**
- 
-   \file MollerGlobalMagnetField.hh
-   $Revision: 1.2 $	
-   $Date: 2005/12/27 19:24:27 $
-   \author Klaus Hans Grimm
-     
-*/
-//=============================================================================
-// 
-//=============================================================================
-// 
-//   ---------------------------
-//  | Doxygen Class Information |
-//  ---------------------------
-/**
-   \class MollerGlobalMagnetField
-    
-   \brief Set in MollerDetectorConstruction as the field manager.
-
- MollerGlobalMagnetField::MollerGlobalMagnetField() sets the field map names, initializes 
- the grid after setting the min and max r, z, phi, and reads in the maps.  
- MollerGlobalMagnetField::GetFieldValue() adds the field value for the hybrid and upstream toroids 
- from MollerMagnetFieldMap::GetFieldValue (which does the interpolation) during tracking.
-
-    
- */
-//=============================================================================
-//
-//=============================================================================
-//   -----------------------
-//  | CVS File Information |
-//  -----------------------
-// 
-//  Last Update:      $Author: grimm $
-//  Update Date:      $Date: 2005/12/27 19:24:27 $
-//  CVS/RCS Revision: $Revision: 1.2 $
-//  Status:           $State: Exp $
-// 
-// ===================================
-//  CVS Revision Log at end of file !!
-// ===================================
-// 
-//=============================================================================
-
 //....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo......
 #ifndef MollerGlobalMagnetField_h
 #define MollerGlobalMagnetField_h 
@@ -60,10 +10,6 @@
 #include "MollerMagnetFieldMap.hh"
 #include "MollerGlobalMagnetFieldMessenger.hh"
 
-// root include
-#include "TGraph.h"
-// user classes
-//class MollerGlobalMagnetFieldMessenger;
 class MollerMagnetFieldMap;
 class MollerGlobalMagnetFieldMessenger;
 
@@ -79,7 +25,8 @@ public:
 
   void SetHybridMapFile(const G4String&);
   void SetUpstreamMapFile(const G4String&);
-  
+
+  void SetConfiguration(const G4String);
   void SetLowLimSeptumField(G4double value) { mg_field_low = value; };
   void SetHighLimSeptumField(G4double value) { mg_field_high = value; };
   void SetScaleFactor(G4double value) {magScaleFactor=value;}
@@ -89,8 +36,10 @@ public:
   G4double GetScaleFactor() {return magScaleFactor;}
 
   void GetFieldValue( const  G4double Point[4], G4double *Bfield ) const;  
+  void GetFieldValuePREX2( const  G4double Point[4], G4double *Bfield ) const;  
+  void GetFieldValueCREX( const  G4double Point[4], G4double *Bfield ) const;  
   void WriteMagField();
-
+  
 private:
 
   //MollerGlobalMagnetFieldMessenger*      fGlobalMagnetFieldMessenger;
@@ -105,6 +54,7 @@ private:
   
   G4String upstreamFilename;
   G4String hybridFilename;
+  G4String configuration;
 
   G4double magScaleFactor;
   G4double mg_field_low;
