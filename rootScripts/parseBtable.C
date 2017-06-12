8550177bcc75f8fd924920cdc8e3e2f1be6f1234
Ciprian Gal <ciprian@jlab.org>
2017-06-12 16:01:09 -0400

    update with different filenames

--- rootScripts/parseBtable.C
+++ rootScripts/parseBtable.C
@@ -1,27 +1,51 @@
 void parseBtable(){
   TGraph *g=new TGraph();
   double x,y,z,bx,by,bz;
-  ifstream fin("../Q1FieldMap-Beamline.table");
+  //ifstream fin("../Q1FieldMap-Beamline.table");
+  //ifstream fin("../PREX_grid.table");
+  //ifstream fin("../CREX_grid.table");
+  //ifstream fin("../septum3od012inPipeInSquare.table");
+  //ifstream fin("../prex_septumShieldPlus3odPipe_quadsWithConicalShieldPipe.table");
+  //ifstream fin("../crex_septumShieldPlus3odPipe_quadsWithConicalShieldPipe.table");
+
+  //ifstream fin("../prex_all_on_170609_properSeptumShield.table");
+  ifstream fin("../crex_all_on_170609_properSeptumShield.table");
+  // ifstream fin("../prex_Q1Q2off_170609_properSeptumShield.table");
+  // ifstream fin("../crex_Q1Q2off_170609_properSeptumShield.table");
+  // ifstream fin("../prex_Q2off_170609_properSeptumShield.table");
+  // ifstream fin("../crex_Q2off_170609_properSeptumShield.table");
+
   char data[500];
-  for(int i=0;i<8;i++)
+  for(int i=0;i<8;i++){
     fin.getline(data,500);
+    cout<<data<<endl;
+  }
   int ng(0);
   int i(0);
-  cout<<"double b[]={";
+  std::vector<double> zPos;
+  std::vector<double> bX;
   while(fin>>x>>y>>z>>bx>>by>>bz){
     i++;
-    //if(i%10000==1) cout<<i<<endl;
-    // if(y==0 && x==1){
-    //   g->SetPoint(ng,z,-by);
-    //   ng++;
-    // }
-    if(x==0 && y==1){
-      g->SetPoint(ng,z,-bx);
-      cout<<bx<<", ";
+    if(fabs(x)<0.03 && fabs(y-1)<0.03){
+      // cout<<x<<" "<<y<<" "<<z<<" "<<bx<<" "<<by<<" "<<bz<<endl;
+      // cout<<endl<<"\t"<<abs(x)<<" -> "<<(abs(x)<0.03)<<" | "<<abs(y-1)<<" -> "<<(abs(y-1)<0.03)<<endl;
+      zPos.push_back(z);
+      bX.push_back(bx);
+      g->SetPoint(ng,z,bx);
       ng++;
     }
   }
+  cout<<" zPosition = {";
+  for(int i=0;i<zPos.size();i++)
+    cout<<" "<<zPos[i]<<",";
   cout<<"};\n";
+
+  cout<<" bXcomponent = {";
+  for(int i=0;i<zPos.size();i++)
+    cout<<" "<<bX[i]<<",";
+  cout<<"};\n";
+
+
   gStyle->SetOptFit(1);
   g->Draw("AL");
   fin.close();
