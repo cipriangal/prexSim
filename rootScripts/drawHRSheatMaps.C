c48cb93e992722023ad2e2899313e76d33aee2a6
Ciprian Gal <ciprian@jlab.org>
2017-08-15 22:17:29 -0400

    add option to draw differente detectors

--- rootScripts/drawHRSheatMaps.C
+++ rootScripts/drawHRSheatMaps.C
@@ -1,4 +1,4 @@
-const int nFl=11;
+const int nFl=19;
 TCanvas *c1[nFl];
 TPaveText *rat[6][nFl];
 double ref[6];
@@ -10,23 +10,35 @@ void drawHRSheatMaps(){
     "../output/p2_1GeV_noQ1_P1dumpV2_5e6.root",
     "../output/p2_1GeV_noQ1_dumpV3_dumpShield_5e6.root",
     "../output/p2_1GeV_noQ1_dumpV3_dumpShieldPoly_5e6.root",
+    "../output/p2_1GeV_noQ1_dumpV3_4inDonut_dumpShieldConc_5e6.root",
+    "../output/p2_1GeV_noQ1_dumpV3_donutShield_5e6.root",
     "../output/c5_2GeV_noQ1_dumpV3_5e6.root",
     "../output/c5_2GeV_noQ1_dumpV3_4inDonut_5e6.root",
     "../output/c5_2GeV_noQ1_P1dumpV2_5e6.root",
     "../output/c5_2GeV_noQ1_dumpV3_dumpShield_5e6.root",
     "../output/c5_2GeV_noQ1_dumpV3_dumpShieldPoly_5e6.root",
-    "../output/c5_2GeV_2PipeSeptum_noQ1_dumpV3_5e6.root"
+    "../output/c5_2GeV_2PipeSeptum_noQ1_dumpV3_5e6.root",
+    "../output/c5_2GeV_noQ1_dumpV3_4inDonut_dumpShield_5e6.root",
+    "../output/c5_2GeV_2PipeSeptum_noQ1_dumpV3_dumpShiedlDonut_5e6.root",
+    "../output/c5_dumpV3_2ftConcDumpShield.root",
+    "../output/c5_dumpV3_0p5ftConcDumpShield.root",
+    "../output/p2_dumpV3_2ftConcDumpShield.root",
+    "../output/p2_dumpV3_0p5ftConcDumpShield.root"
   };
   for(int i=0;i<nFl;i++){
-  //for(int i=0;i<2;i++){
+  //for(int i=15;i<nFl;i++){
+    if(i!=0 && i!=7 && i!=3 && i!=8) continue;
+    cout<<i<<": "<<file[i].c_str()<<endl;
     c1[i]=new TCanvas(Form("c%d",i),file[i].c_str(),800,1000);
     c1[i]->Divide(2,3);
-    //drawOneHRSheatMaps(file[i],c1[i],i);
-    drawNEILdmgOne(file[i],c1[i],i);
+    //drawOneHeatMaps(file[i],c1[i],i,1001);//hrs
+    //drawNEILdmgOne(file[i],c1[i],i,1001);//hrs
+    drawOneHeatMaps(file[i],c1[i],i,1005);//under
+    //drawNEILdmgOne(file[i],c1[i],i,1005);//under
   }
 }
 
-void drawOneHRSheatMaps(string fnm,TCanvas *can, int nF){
+void drawOneHeatMaps(string fnm,TCanvas *can, int nF, int det){
   TFile *fin=TFile::Open(fnm.c_str(),"READ");
   TNtuple *t=(TNtuple*)fin->Get("geant");
 
@@ -47,13 +59,13 @@ void drawOneHRSheatMaps(string fnm,TCanvas *can, int nF){
 
   for(int i=0;i<6;i++){
     h->SetTitle(cuts[i].c_str());
-    t->Project("h","x0:z0",Form("volume==1001 && %s",cuts[i].c_str()));
+    t->Project("h","x0:z0",Form("volume==%d && %s",det,cuts[i].c_str()));
     can->cd(i+1);
 
     h->DrawCopy("colz");
     double entries=h->Integral();
     rat[i][nF]=new TPaveText(0.2,0.8,0.3,0.9,"NDC");
-    if(nF==0 || nF==5){
+    if(nF==0 || nF==7){
       ref[i]=entries;
     }else{
       rat[i][nF]->Clear();
@@ -69,7 +81,7 @@ void drawOneHRSheatMaps(string fnm,TCanvas *can, int nF){
   fin->Close();
 }
 
-void drawNEILdmgOne(string fnm,TCanvas *can, int nF){
+void drawNEILdmgOne(string fnm,TCanvas *can, int nF, int det){
   string fnmHalRad = fnm.substr(0, fnm.length()-5) + "_hallRad.root";
   cout<<fnmHalRad<<endl;
   TFile *fin=TFile::Open(fnmHalRad.c_str(),"READ");
@@ -77,8 +89,8 @@ void drawNEILdmgOne(string fnm,TCanvas *can, int nF){
   gStyle->SetStatW(0.3);
   gStyle->SetStatH(0.2);
 
-  TH1D *he=(TH1D*)fin->Get("Det_1001/ha_1001_e_neilLinX");
-  TH1D *hn=(TH1D*)fin->Get("Det_1001/ha_1001_n_neilLinX");
+  TH1D *he=(TH1D*)fin->Get(Form("Det_%d/ha_%d_e_neilLinX",det,det));
+  TH1D *hn=(TH1D*)fin->Get(Form("Det_%d/ha_%d_n_neilLinX",det,det));
 
   int nC[6]={1,3,5,2,4,6};
   double lims[4]={0,0.1,10,2000};
