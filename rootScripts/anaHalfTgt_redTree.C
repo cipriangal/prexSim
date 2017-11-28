a8bd8158ed59cad38bcd7f3b4051e7dcf4384794
Ciprian Gal <ciprian@jlab.org>
2017-11-28 15:58:20 -0500

    redesing macro. does the same thing as before

--- rootScripts/anaHalfTgt_redTree.C
+++ rootScripts/anaHalfTgt_redTree.C
@@ -2,45 +2,72 @@ void anaHalfTgt_redTree(){
 
   // anaHalfTgt_redTreeOne("../output/dump_crex5_newHRS_newDet/file_redTree.root");
   // anaHalfTgt_redTreeOne("../output/dump_prexII_fullLead_4inDonut/file_redTree.root");
-  // anaHalfTgt_redTreeOne("../output/dump_prexII_halfLead/file_redTree.root");
-  //anaHalfTgt_redTreeOne("../output/dump_prexII_newHRS_newDet/file_redTree.root");
-  anaHalfTgt_redTreeOne("../output/dump_prexII_0p75Tgt/file1_redTree.root");
+  //anaHalfTgt_redTreeOne("../output/dump_prexII_halfLead/file_redTree.root");
+  //anaHalfTgt_redTreeOne("/home/ciprian/hdd2/prexOutput/dump_prexII_newHRS_newDet/file_redTree.root");
+  anaHalfTgt_redTreeOne("../output/dump_prexII_0p75Tgt/file5_redTree.root");
 }
 void anaHalfTgt_redTreeOne(string fnm){
   cout<<"opening "<<fnm<<endl;
   TFile *fin=TFile::Open(fnm.c_str(),"READ");
   TTree *t=(TTree*)fin->Get("t");
-  string ofnm=Form("%s_anaRedTree.root",fnm.substr(0,fnm.find_last_of(".")).c_str());
+  string ofnm=Form("%s_anaRedTTree.root",fnm.substr(0,fnm.find_last_of(".")).c_str());
   cout<<"\twriting to "<<ofnm<<endl;
   TFile *fout=new TFile(ofnm.c_str(),"RECREATE");
 
   TH2D *h1=new TH2D("h1","heatmap for volID=1001;z [mm];x [mm]"     ,200,25000,36000,200,-2000,2000);
   TH2D *h2=new TH2D("h2","NEIL heatmap for volID=1001;z [mm];x [mm]",200,25000,36000,200,-2000,2000);
-  t->Project("h1","x0:z0","volID==1001 && E>0.1","");
-  t->Project("h2","x0:z0","neil*(volID==1001 && E>0.1 && neil>0)");
-
-  TH1D *h5=new TH1D("h5","NEIL weighted distribution for volID=1001;origin z[mm]",
-		    200,25000,36000);
-  t->Project("h5","z0","neil*(volID==1001 && E>0.1 && neil>0)");
-
-  TH1D *h6=new TH1D("h6","NEIL weighted distribution for volID=1001 e^#pm;origin z[mm]",
-		    200,25000,36000);
-  t->Project("h6","z0","neil*(volID==1001 && E>0.1 && neil>0 && abs(pdgID)==11)");
-
-  TH1D *h7=new TH1D("h7","NEIL weighted distribution for volID=1001 neutrons;origin z[mm]",
-		    200,25000,36000);
-  t->Project("h7","z0","neil*(volID==1001 && E>0.1 && neil>0 && pdgID==2112)");
-
-  TH1D *h3[8];
+  TH1D *h3[8],*h4[8];
   for(int i=0;i<8;i++){
     h3[i]=new TH1D(Form("h3_%d",i),Form("radial distribution for det %d; r[cm]",2201+i),600,0,300);
-    t->Project(Form("h3_%d",i),"sqrt(x*x+y*y)",Form("volID==%d && trackID==1 && parentID==0 && pdgID==11",2201+i));
-  }
-
-  TH1D *h4[8];
-  for(int i=0;i<8;i++){
     h4[i]=new TH1D(Form("h4_%d",i),Form("radial distribution for e^#pm and #gamma det %d (E>100MeV); r[cm]",2201+i),600,0,300);
-    t->Project(Form("h4_%d",i),"sqrt(x*x+y*y)",Form("E>100 && volID==%d && trackID==1 && parentID==0 && (abs(pdgID)==11 || abs(pdgID)==22)",2201+i));
+  }
+  TH1D *h5=new TH1D("h5","NEIL weighted distribution for volID=1001;origin z[mm]",
+		    200,25000,36000);
+  TH1D *h6=new TH1D("h6","NEIL weighted distribution for volID=1001 e^#pm;origin z[mm]",
+		    200,25000,36000);
+  TH1D *h7=new TH1D("h7","NEIL weighted distribution for volID=1001 neutrons;origin z[mm]",
+		    200,25000,36000);
+
+  float x0,z0,x,y,E,neil;
+  int pdgID,volID,tID,pID;
+  t->SetBranchAddress("pdgID",&pdgID);
+  t->SetBranchAddress("trackID",&tID);
+  t->SetBranchAddress("parentID",&pID);
+  t->SetBranchAddress("volID",&volID);
+  t->SetBranchAddress("x0",&x0);
+  t->SetBranchAddress("z0",&z0);
+  t->SetBranchAddress("x",&x);
+  t->SetBranchAddress("y",&y);
+  t->SetBranchAddress("E",&E);
+  t->SetBranchAddress("neil",&neil);
+
+  long nEntries=t->GetEntries();
+  float currentProc=1,procStep=10;
+  for(long k=0;k<nEntries;k++){
+    t->GetEntry(k);
+    if( float(k+1)/nEntries*100 > currentProc){
+      cout<<"at tree entry\t"<<k<<"\t"<< float(k+1)/nEntries*100<<" %"<<endl;
+      currentProc+=procStep;
+    }
+    if(E<=0.1) continue;
+
+    if(volID==1001){
+      h1->Fill(z0,x0);
+      if(neil>0){
+	h2->Fill(z0,x0,neil);
+	h5->Fill(z0,neil);
+	if(abs(pdgID)==11)
+	  h6->Fill(z0,neil);
+	else if(pdgID==2112)
+	  h7->Fill(z0,neil);
+      }
+    }else if(volID>2200 && volID<2209){
+      int det = volID - 2201;
+      if(tID==1 && pID==0 && pdgID==11)
+	h3[det]->Fill(sqrt(x*x+y*y));
+      else if(E>100 && (abs(pdgID)==11 || pdgID==22))
+	h4[det]->Fill(sqrt(x*x+y*y));
+    }
   }
 
   fout->cd();
@@ -54,11 +81,4 @@ void anaHalfTgt_redTreeOne(string fnm){
     h4[i]->Write();
   }
   fout->Close();
-  // delete h1;
-  // delete h2;
-  // delete h5;
-  // delete h6;
-  // delete h7;
-  // delete[] h3;
-  // delete[] h4;
 }
